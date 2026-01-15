c8899a4af237f032740b834b161ae2e62bc31f7b
Vedant Khairnar <vedant3khairnar@gmail.com>
2026-01-15 12:58:05 +0530

    Page 1 Done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'user_form14-app';
+
+  userForm: FormGroup;
+  message = '';
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      age: ['', [Validators.required, Validators.min(18), Validators.max(60)]],
+      gender: ['', Validators.required]
+    });
+
+  }
+
+  submitForm() {
+
+    if (this.userForm.invalid) {
+      return;
+    }
+
+    this.http.post('http://localhost:8000/api/users/', this.userForm.value)
+      .subscribe({
+        next: () => {
+          this.message = 'Form submitted successfully';
+          this.userForm.reset();
+        },
+        error: () => {
+          this.message = 'Error submitting form';
+        }
+      });
+  }
 }
